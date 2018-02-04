960e6329411c43fb231c2c8db8c4f37e460615db
mmJ <mmjxxxx@163.com>
2018-02-04 23:31:46 +0800

    2018-02-04 23:30

--- src/ServerII/ChatSocket.java
+++ src/ServerII/ChatSocket.java
@@ -7,8 +7,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ChatSocket extends Thread {
-    String filename="s1.c";
-    File f1 = new File("/home/mmj/Documents/"+filename);
+   // String filename="s1.c";
+   // File f1 = new File("/home/mmj/Documents/"+filename);
     Socket socket;
     BufferedWriter bw;
     BufferedReader br;
@@ -26,7 +26,7 @@ public class ChatSocket extends Thread {
         }
     }
     //发送数据
-    public void out(String out) {
+    public  void out(String out) {
         try {
             bw.write(out + "\n");// 必须要加换行符号,不然数据发不出去
             bw.flush();
@@ -41,12 +41,16 @@ public class ChatSocket extends Thread {
     public void run() {
         try {
             String line = null;
+            out("***感谢参与测试...您在此app所发送信息将传输至服务器.若您与其他用户（不限个数）输入相同密码并在同一时间段内发送消息将实现聊天功能...此app仍在初始编写测试阶段");
+            out("***app将在更新后第一时间发布至www.mamaji.xin");
+            out("***联系方式 email: mmjxxxx@163.com");
+            out("***再次感谢您的参与");
             while ((line = br.readLine()) != null)
             {//监听客户端发来的数据
                // System.out.println("客户端发来数据："+line);
                // MyServerSocket.addtext(dateFormat.format(date.getTime()));
                 MyServerSocket.addtext(dateFormat.format(date.getTime())+" "+"Reveived from Client："+line);
-                datasave(f1,line);
+                //datasave(f1,line);
                 //把数据发给其余的客户端
                 ChatManager.getChatManager().publish(this, line);
 
--- src/TEST.java
+++ src/TEST.java
@@ -12,40 +12,35 @@ public class TEST {
             e.printStackTrace();
         }
         BufferedWriter writer = null;
+        BufferedReader reader = null;
         try {
-            writer = new BufferedWriter( new OutputStreamWriter(
-                    socket.getOutputStream(), "utf-8" ) );
+            writer = new BufferedWriter(new OutputStreamWriter(
+                    socket.getOutputStream(), "utf-8"));
+            reader = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream(), "utf-8"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        try {
-//            //1.创建客户端Socket，指定服务器地址和端口
-//            InputStreamReader isr = null;
-//            Socket socket = new Socket("localhost", 7400);
-//            byte[] sendMessage = ("As the moon,so beautiful!\n" ).getBytes();
-//            OutputStream out = socket.getOutputStream();
-//            out.write(sendMessage);
-//            isr = new InputStreamReader(socket.getInputStream());
-//            System.out.println(new BufferedReader(isr).readLine());
-//            socket.close();
-//        } catch (UnknownHostException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-        BufferedWriter finalWriter = writer;
-        new Thread(new Runnable(){
-            @Override
-            public void run(){
-                try {
-                    String sendData ="***今晚月色真美！";
-                    finalWriter.write(sendData + "\n");//必须加上换行
-                    finalWriter.flush();
-                } catch (IOException e) {
-                    System.out.println("Error!");
-                    e.printStackTrace();
-                }
+            BufferedWriter finalWriter = writer;
+            BufferedReader finalReader = reader;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        String sendData = "***今晚月色真美！";
+                        finalWriter.write(sendData + "\n");//必须加上换行
+                        finalWriter.flush();
+                        finalWriter.write(sendData + "\n");//必须加上换行
+                        finalWriter.flush();
+                        finalWriter.write(sendData + "\n");//必须加上换行
+                        finalWriter.flush();
+                        System.out.println(finalReader.readLine());
+                    } catch (IOException e) {
+                        System.out.println("Error!");
+                        e.printStackTrace();
+                    }
 
-            }
-        }).start();
+                }
+            }).start();
         }
     }
